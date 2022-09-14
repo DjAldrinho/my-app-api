4554a64c91c0f747c95142e9920d820f28fbc8e4
DjAldrinho <aldray.devsoft@gmail.com>
2022-09-13 20:12:41 -0500

    first commit

---
+++ src/main/java/com/devsoft/myhotelapi/MyHotelApiApplication.java
@@ -0,0 +1,13 @@
+package com.devsoft.myhotelapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHotelApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyHotelApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devsoft/myhotelapi/MyHotelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsoft.myhotelapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHotelApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
